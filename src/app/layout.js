87d8d66e7c736577f233a7280538aed8bf53cc3f
Sagor <mojahidulislamsagor1234@gmail.com>
2026-05-10 10:36:43 +0600

    Banner Section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
+import Banner from "@/components/Banner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         <NavBar></NavBar>
+        <Banner></Banner>
         {children}
       </body>
     </html>
